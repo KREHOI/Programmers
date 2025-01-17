e234f677b0f76bf03dbc8ec4c00e20cbed2fc98e
Park Tae I <155876292+KREHOI@users.noreply.github.com>
2025-01-17 15:02:23 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 62.94 ms, Memory: 47.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.cs
@@ -0,0 +1,22 @@
+using System;
+
+public class Solution {
+    public int[] solution(string s) {
+        int[] answer = new int[s.Length];
+        
+        for (int idx=0; idx<s.Length; idx++)
+        {
+            char ch = s[idx];
+            string targetStr = s.Substring(0, idx);
+            
+            int findIdx = targetStr.LastIndexOf(ch);
+            
+            if (findIdx == -1)
+                answer[idx] = findIdx;
+            else
+                answer[idx] = idx - findIdx;
+        }
+        
+        return answer;
+    }
+}
